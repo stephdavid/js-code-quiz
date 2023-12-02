78a86d8c165a05d16f071c944f22a64748c1f2fe
Stephanie David <stephdaviduk@gmail.com>
2023-12-02 12:10:14 +0000

    Using local storage and web API methods getting the current game score and game count and displaying them on the highscores.html page - still more work nedded

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,12 +3,23 @@ const ol = document.getElementById("highscores");
 let highestScore = 0;
 let gameScore = ""
 
-const highScore1Li = document.createElement("li");
-const highScore2Li = document.createElement("li");
-const highScore3Li = document.createElement("li");
-const highScore4Li = document.createElement("li");
-const highScore5Li = document.createElement("li");
+const highscore1 = document.createElement("li");
+const highscore2 = document.createElement("li");
+const highscore3 = document.createElement("li");
+const highscore4 = document.createElement("li");
+const highscore5 = document.createElement("li");
 
+highscores.appendChild(highscore1);
+highscores.appendChild(highscore2);
+highscores.appendChild(highscore3);
+highscores.appendChild(highscore4);
+highscores.appendChild(highscore5);
+
+highscore1.setAttribute("id", "highscore1");
+highscore2.setAttribute("id", "highscore2");
+highscore3.setAttribute("id", "highscore3");
+highscore4.setAttribute("id", "highscore4");
+highscore5.setAttribute("id", "highscore5");
 
 let latestScore = localStorage.getItem("gameScore");
 console.log(latestScore);
@@ -19,8 +30,8 @@ console.log(latestGameCount);
 function displayFiveHighestScores() {
 
     if (latestGameCount < 5) {
-            //push latest score to the array
-            scores.push(latestScore);
+        //push latest score to the array
+        scores.push(latestScore);
     } else {
         //push latest score to the to the array
         scores.push(latestScore);
@@ -29,13 +40,17 @@ function displayFiveHighestScores() {
             return b - a
         });
         //remove the smallest value in the array
-        scores.slice(-1,1) // remove only the last element - supposedly faster than pop
+        scores.slice(-1, 1) // remove only the last element - supposedly faster than pop
 
     }
-    for (let i = 0; i <= scores.length; i++) {
-        document.getElementsByClassName("highscore")[i].textContent = scores[i];
-    }
+    // Update the highscore list
+    highscore1.textContent = scores[0] || "";
+    highscore2.textContent = scores[1] || "";
+    highscore3.textContent = scores[2] || "";
+    highscore4.textContent = scores[3] || "";
+    highscore5.textContent = scores[4] || "";
+
 }
 
 displayFiveHighestScores();
-    
+
